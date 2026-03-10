57e151655e88b78aec82c7b4bc0aca0f495455a4
allen <allen@momomo.com>
2026-03-10 16:29:19 +0800

    add Gender field and more constructors

--- PersonLib/Person.h
+++ PersonLib/Person.h
@@ -1,13 +1,17 @@
-#pragma once
+﻿#pragma once
 #include<string>
 class Person
 {
+public:
+	enum Gender{MALE, FEMALE, NONBINARY};
 private:
 	int age;
 	std::string name;
+	Gender gender;
 public:
 	std::string getName();
 	int getAge();
+	Gender getGender();
 
 	Person(std::string n, int age);
 	Person(std::string n);
